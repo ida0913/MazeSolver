2ae82147a41857e1c9d47a408e202aa4c3edb094
Adi <adib09132005@gmail.com>
2022-09-13 00:51:15 -0500

    complete

--- src/Maze.java
+++ src/Maze.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.swing.SpinnerDateModel;
+
+import java.util.*;
+
 public class Maze {
     private Square[][] maze;
-    private Square start;
-    private Square end;
+    private int[][] testMaze;
 
     public boolean loadMaze(String filename) {
 
@@ -19,24 +22,24 @@ public class Maze {
             int rows = scanner.nextInt();
             int cols = scanner.nextInt();
             this.maze = new Square[rows][cols];
-
+            testMaze = new int[rows][cols];
             // iterate thru maze
             for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; cols++) {
-                    maze[i][j] = new Square(i, j, scanner.nextInt());
-                }
-            }
-            for (int i = 0; i < maze.length; i++) {
-                for (int j = 0; i < maze[0].length; j++) {
-                    if (maze[i][j].getType() == 2) {
-                        start = maze[i][j];
-                    } else if (maze[i][j].getType() == 3) {
-                        end = maze[i][j];
-                    }
-                }
-            }
+                for (int j = 0; j < cols; j++) {
+                    int temp = scanner.nextInt();
+                    maze[i][j] = new Square(i, j, temp);
+                    testMaze[i][j] = temp;
 
+                }
+            }
+            for (int[] x : testMaze) {
+                for (int y : x) {
+                    System.out.print(y + " ");
+                }
+                System.out.println();
+            }
             scanner.close();
+
             return true;
         } catch (FileNotFoundException e) {
             System.out.println("Your fucking file isn't there");
@@ -49,22 +52,39 @@ public class Maze {
 
         int row = s.getRow();
         int col = s.getCol();
+        System.out.println(row);
+        System.out.println(col);
 
         ArrayList<Square> neighbors = new ArrayList<>();
+        if (row - 1 > 0) {
+            if (maze[row - 1][col] != null) {
+                neighbors.add(maze[row - 1][col]);
+            }
+        }
 
-        if (maze[row - 1][col] != null) {
-            neighbors.add(maze[row - 1][col]);
+        if (row + 1 < maze.length) {
+            if (maze[row + 1][col] != null) {
+                neighbors.add(maze[row + 1][col]);
+
+            }
         }
-        if (maze[row + 1][col] != null) {
-            neighbors.add(maze[row + 1][col]);
+
+        if (col - 1 > 0) {
+            if (maze[row][col - 1] != null) {
+                neighbors.add(maze[row][col - 1]);
+            }
         }
-        if (maze[row][col - 1] != null) {
-            neighbors.add(maze[row][col - 1]);
+
+        if (col + 1 < maze[0].length) {
+            if (maze[row][col + 1] != null) {
+                neighbors.add(maze[row][col + 1]);
+            }
         }
-        if (maze[row][col + 1] != null) {
-            neighbors.add(maze[row][col + 1]);
-        }
-        return neighbors;
+
+    
+
+    return neighbors;
+
     }
 
     public void reset() {
@@ -89,10 +109,26 @@ public class Maze {
     }
 
     public Square getStart() {
-        return start;
+        for (int i = 0; i < testMaze.length; i++) {
+            for (int j = 0; j < testMaze[0].length; j++) {
+
+                if (testMaze[i][j] == 2) {
+                    return maze[i][j];
+                }
+            }
+        }
+        return null;
     }
 
     public Square getEnd() {
-        return end;
+        for (int i = 0; i < testMaze.length; i++) {
+            for (int j = 0; j < testMaze[0].length; j++) {
+                if (testMaze[i][j] == 3) {
+                    return maze[i][j];
+                }
+            }
+        }
+        return null;
     }
+
 }
